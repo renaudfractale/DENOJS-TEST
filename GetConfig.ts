3c671ff3d398eb28da291c5b6d40e39263c926d4
Renaud <git@rhenry.fr>
2021-05-17 21:13:40 +0200

    Génération Conf JSON OK

---
+++ GetConfig.ts
@@ -0,0 +1,3 @@
+import { parse } from "https://deno.land/std/flags/mod.ts";
+let Args : any = parse(Deno.args)
+console.dir(parse(Deno.args));
--- cmdtest.cmd
+++ cmdtest.cmd
@@ -1,1 +1,1 @@
-deno run .\CreateConfig.ts --c position --Q.x.start "\-1.5" --Q.y.start "\-1.5" --Q.z.start "\-1.5" --Q.w.start "\-1.5"  --Q.x.end 1.5 --Q.y.end 1.5 --Q.z.end 1.5 --Q.w.end 1.5  --Q.x.axeplot x --Q.y.axeplot y --Q.z.axeplot z --Q.w.axeplot t
+deno run .\GetConfig.ts --file .\conf\config1.json
